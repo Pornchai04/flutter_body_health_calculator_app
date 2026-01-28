eed01bf2a6eda90941e71436b42f0973e68e8607
kaniop <kitsanapon_pin@hotmail.com>
2026-01-28 12:30:51 +0700

    commit03 :ทำ NavigationBar Home Ui เสร็จแล้ว

---
+++ lib/views/abuot_ui.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AbuotUi extends StatefulWidget {
+  const AbuotUi({super.key});
+
+  @override
+  State<AbuotUi> createState() => _AbuotUiState();
+}
+
+class _AbuotUiState extends State<AbuotUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text('Abuot'),
+    ));
+  }
+}
---
+++ lib/views/splash_screen_ui.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_body_health_calculator_app/views/home_ui.dart';
+
+class SplashScreenUi extends StatefulWidget {
+  const SplashScreenUi({super.key});
+
+  @override
+  State<SplashScreenUi> createState() => _SplashScreenUiState();
+}
+
+class _SplashScreenUiState extends State<SplashScreenUi> {
+  @override
+  void initState() {
+    // ให้หน่วงเวลา 3 วินาที กแล้วเปิดไปหน้า HomeUi แบบย้อนกลับไม่ได้
+    Future.delayed(
+      //เวลาที่หน่วง
+      Duration(seconds: 3),
+      //เมื่อครบเวลาแล้วให้ทำงานอะไร ณ ที่นี้จะให้เปิดหน้า HomeUi แบบย้อนกลับไม่ได้
+      () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomeUi(),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 7, 234),
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/calculator.png',
+              width: MediaQuery.of(context).size.width * 0.50,
+              height: MediaQuery.of(context).size.width * 0.50,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Text(
+              'Body Health Calculator',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.025,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
